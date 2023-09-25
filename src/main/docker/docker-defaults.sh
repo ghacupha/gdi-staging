9d9659f3afff89f1dca326442aafe85178a060dd
Edwin Njeru <mailnjeru@gmail.com>
2023-09-25 15:07:15 +0300

    Additional deployment logs

--- src/main/docker/docker-defaults.sh
+++ src/main/docker/docker-defaults.sh
@@ -40,6 +40,10 @@ echo "Using nginx configurations on /etc/nginx/conf.d/"
 
 ls -la /etc/nginx/conf.d/default.conf
 
+echo "Launching compilation content on /usr/share/nginx/"
+
+ls -la /usr/share/nginx/
+
 # Finally, let the original Nginx entry point do its work, passing whatever is
 # set for CMD. Use `exec` to replace the current process, to trap any signals
 # (like Ctrl+C) that Docker may send it:
